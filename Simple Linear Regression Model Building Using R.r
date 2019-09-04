6357fe1196405e5e80806b27cbfa59164cd64207
PRAKASHANAND <34940322+GHATAK123@users.noreply.github.com>
2019-09-05 01:11:54 +0530

    Add files via upload

---
+++ Simple Linear Regression Model Building Using R.r
@@ -0,0 +1,10 @@
+
+bonds <- read.delim('bonds.txt',row.names=1)
+
+head(bonds)
+
+tail(bonds)
+
+str(bonds) # This function shows each variables with their corresponding data types or structure of dataframe
+
+
